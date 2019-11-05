be710f0548d06f87497a9dd1b6a18a4967c2dcf3
hdknr <gmail@hdknr.com>
2019-11-05 18:53:23 +0900

    dispatcher

--- layersclick/__init__.py
+++ layersclick/__init__.py
@@ -1,25 +1,18 @@
 __version__ = '0.0.1'
 import click
+import pkgutil
+import importlib
 import sys
-from .logs import logs
-from .ec2 import ec2
-from .mails import mails
-from .utils import setup
-from .builder import builder
-
-
-COMMANDS = {
-    'ec2': ec2, 'mails': mails, 'logs': logs,
-    'builder': builder,
-}
 
 
 def dispatch():
+    BASE = __name__
     if len(sys.argv) >= 2:
-        command = COMMANDS.get(sys.argv[1], None)
-        if command:
+        name = sys.argv[1]
+        try:
+            mod = importlib.import_module(f"{BASE}.{name}")
             del sys.argv[1]
-            command(obj={})
-
-    click.echo("specify commad group:\n")
-    click.echo(",".join(COMMANDS.keys()))
+            getattr(mod, name)(obj={})
+            return
+        except:
+            pass
