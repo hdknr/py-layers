0ff48a4258c75264285b42609c3f6c2bde668169
user.name <gmail@hdknr.com>
2020-08-02 08:21:34 +0900

    ArgumentParser

--- layersclick/__init__.py
+++ layersclick/__init__.py
@@ -1,15 +1,32 @@
 __version__ = '0.0.1'
-import click
-import pkgutil
-import importlib
-import sys
+
+
+def subcommands():
+    import glob
+    import os
+    return [
+        os.path.basename(i).replace('.py', '') 
+        for i in glob.glob(os.path.dirname(__file__) + '/*.py')
+        if i.find('__init__.py') < 0
+    ]
+
+
+def get_subcommand():
+    import argparse
+    parser = argparse.ArgumentParser(description='AWS Command Tools Set', add_help=False)
+    parser.add_argument('subcommand',  choices=subcommands())
+    args, unkonwns = parser.parse_known_args()
+    return args.subcommand
 
 
 def dispatch():
+    import importlib
+    import sys
+
     BASE = __name__
     func = None
     if len(sys.argv) >= 2:
-        name = sys.argv[1]
+        name = get_subcommand()
         try:
             mod = importlib.import_module(f"{BASE}.{name}")
             del sys.argv[1]
